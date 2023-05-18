b4e699e3c1657b01f09e99bf6b6afd61f81bdf38
Najma <najmathande@gmail.com>
2023-05-18 22:52:33 +0300

    made changes

--- app.rb
+++ app.rb
@@ -1,16 +1,16 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+ pp [{ id: 1, hello: "World" }, 
+     { id: 2, hello: "Ruby" }, 
+     { id: 3, hello: "Moon" }, 
+    { id: 4, hello: "Learner" }]
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+p [1,2,3]
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
 
-# def add
+def add
     
-# end
+end
 # # 1 + 1
 # puts add + add
 
